2cafedd0d620dc6d409dd51136dabee6e0c3a632
GribanovaNastya <hafizova-i@mail.ru>
2020-02-19 00:45:54 +0300

    Tests are deleted

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,9 @@
 import structure.Structure;
 import structure.Structure.Node;
 
-import java.util.List;
-
-import static structure.Structure.Type.COUNTRY;
-import static structure.Structure.Type.REGION;
-
 public class Main {
 
     public static void main(){
-        List<Node> russianChildren = null;
-        russianChildren.add(Node.builder().name("Bashkortostan").type(REGION).priority(0).build());
-        Node.builder().name("Russia").type(COUNTRY).priority(5).children(russianChildren).build();
+
     }
 }
